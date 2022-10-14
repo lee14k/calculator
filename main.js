7d2a1fdc78153de5fbd8f21fbf9c1d02bc0e24f2
lee14k <kmkovach@mtu.edu>
2022-10-14 13:36:31 -0400

    enabled concatenation of numbers, separated operations into extra.js temporarily

--- main.js
+++ main.js
@@ -1,36 +1,47 @@
 let numbers = document.getElementsByClassName("number")
-let firstNumber
+let addition
+let subtraction
+let multiplication
+let division
+
+let operations = document.getElementsByClassName("operation")
+
+let firstNumber = "0"
 let secondNumber
 let currentOperation
 let result
 let firstNumberDisplay = document.getElementById('display');
+let firstNumberValue
 let arr = Array.from(numbers);
+
+
+//make an array out of the number buttons
+//add an event listener so when they are clicked, the value of the button clicked is saved to firstNumber
+
+
 arr.forEach(numbers => numbers.addEventListener('click', (e) => {
     firstNumber = e.target.value
-    console.log(firstNumber)
 
+    // let tea = firstNumberDisplay.append(firstNumber)
+    console.log(firstNumber + firstNumberDisplay.append(firstNumber))
+
+    let tea = firstNumberDisplay.textContent
+    console.log(tea)
+    //tea = 
+    //console.log(tea)
+    //secondNumber = e.target.value
+
+    //firstNumber.appendNumber(firstNumber.innerText)
 }))
 
+/*} else {
+    arr.forEach(numbers => numbers.addEventListener('click', (e) => {
+        secondNumber = e.target.value
+        firstNumberDisplay.textContent = firstNumber + secondNumber
 
-//function pressed() {
-//}
+        console.log(firstNumber)
+        console.log(secondNumber)
+    }))
 
-function add(a, b) {
-    return a + b
 }
-
-function subtract(a, b) {
-    return a - b
-}
-
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    if (b === 0) {
-        return "can't divide by zero"
-    } else {
-        a / b
-    }
-}
+console.log(firstNumberDisplay)*/
