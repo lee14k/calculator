b573b8a130fa92bd5e480428cbe0191c1c91a052
Kailee Kovach <kmkovach@mtu.edu>
2022-10-21 17:16:10 -0400

    got main functionality working, added style sheet

--- main.js
+++ main.js
@@ -1,41 +1,45 @@
 const calculator = document.querySelector('.calculator')
 let numbers = document.getElementsByClassName("number")
-let addition
-let subtraction
-let multiplication
-let division
-
+let display = document.getElementById('display')
+let equals = document.getElementById('equals')
 let operations = document.getElementsByClassName("operation")
+let clear = document.getElementById('clear')
+let sign=document.getElementById('sign')
 
 let firstNumber
 let first = []
+let second = []
 let secondNumber
 let currentOperation
 let result
 let firstNumberDisplay = document.getElementById('display');
-let firstNumberValue
+let newFirst
+let newSecond
 let arr = Array.from(numbers);
+
+function add(a, b) {
+    display.innerText = ( a + b ) 
+}
+function subtract(a, b) {
+    display.innerText =  a - b
+}
+
+function multiply(a, b) {
+    display.innerText =  a * b
+}
+
+function divide(a, b) {
+    if (a === 0) {
+        display.innerText =  "can't divide by zero"
+    } else {
+        display.innerText =    a / b
+    }
+}
 
 
 //make an array out of the number buttons
 //add an event listener so when they are clicked, the value of the button clicked is saved to firstNumber
 
-
-arr.forEach(numbers => numbers.addEventListener('click', (e) => {
-    firstNumber = e.target.value
-
-    // let tea = firstNumberDisplay.append(firstNumber)
-    first.push(firstNumber)
-    console.log(first)
-
-    //let firstNumberValue = firstNumberDisplay.textContent
-    //console.log(firstNumberValue)
-    //tea = 
-    //console.log(tea)
-    //secondNumber = e.target.value
-
-    //firstNumber.appendNumber(firstNumber.innerText)
-}))
 
 let operator = Array.from(operations);
 
@@ -43,7 +47,7 @@ let operator = Array.from(operations);
 operator.forEach(operations => operations.addEventListener('click', (e) => {
 
     currentOperation = e.target.value
-    if (currentOperation === "+") {
+if (currentOperation === "+") {
         console.log("add")
     } else if (currentOperation === "-") {
         console.log('subtract')
@@ -56,3 +60,63 @@ operator.forEach(operations => operations.addEventListener('click', (e) => {
 }))
 
 
+//if operator is pressed
+//collect new array
+  // let tea = firstNumberDisplay.append(firstNumber)
+   
+    //let firstNumberValue = firstNumberDisplay.textContent
+    //console.log(firstNumberValue)
+    //tea = 
+    //console.log(tea)
+    //secondNumber = e.target.value
+
+    //firstNumber.appendNumber(firstNumber.innerText)
+
+    
+
+arr.forEach(numbers => numbers.addEventListener('click', (e) => {
+    if (!currentOperation=="") {
+        firstNumber = e.target.value
+        first.push(firstNumber)
+         newFirst = parseFloat(first.join(""))
+        console.log(newFirst)
+        display.innerText=newFirst
+    } else {
+        secondNumber = e.target.value
+        second.push(secondNumber)
+        console.log(second)
+         newSecond = parseFloat(second.join(""))
+        console.log(newSecond)
+        display.innerText=newSecond
+
+    }
+
+
+  
+}))
+
+equals.addEventListener('click', (e) =>{
+    if (currentOperation==='+')
+    add(newFirst, newSecond)
+    if (currentOperation==='-')
+    subtract(newFirst, newSecond)
+    if (currentOperation==='*')
+    multiply(newFirst, newSecond)
+    if (currentOperation==='/')
+    divide(newFirst, newSecond)
+})
+
+clear.addEventListener('click', (e) =>{
+    display.innerText="Display!"
+    firstNumber=""
+    first=[]
+    console.log(newFirst)
+    secondNumber=""
+    second=[]
+    console.log(newSecond)
+    currentOperation=""
+})
+
+sign.addEventListener('click', (e)=>{
+    
+})
