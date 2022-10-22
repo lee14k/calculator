6c083d15415b04be88ef2fd74f4877d92738dbc8
lee14k <kmkovach@mtu.edu>
2022-10-21 20:21:44 -0400

    Update extra.js

--- extra.js
+++ extra.js
@@ -1,20 +1,3 @@
-function add(a, b) {
-    return a + b
-}
 
-function subtract(a, b) {
-    return a - b
-}
 
-function multiply(a, b) {
-    return a * b
-}
-
-function divide(a, b) {
-    if (b === 0) {
-        return "can't divide by zero"
-    } else {
-        a / b
-    }
-}
 
